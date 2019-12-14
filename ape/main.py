b9f78c2954b72667e9a552c930d389cb584dcf9a
Scott Russell <me@scottrusell.net>
2019-12-14 08:48:30 -0600

    stats module

--- ape/main.py
+++ ape/main.py
@@ -4,7 +4,7 @@ from random import random, randint
 from math import sqrt, pow, floor, log, ceil
 from multiprocessing import Queue, Pool, Process, Pipe, cpu_count
 
-from blist import sortedlist
+from blist import sortedlist, blist
 
 
 def evaluator(chrm, waiting, nursery):
@@ -22,17 +22,20 @@ def manager(pop, terminate, waiting, nursery, pipe_to, batch_size):
     start_time = time()
     evals = 0
     while not terminate(evals, pop):
-        if evals > 0 and evals % len(pop) == 0:
+        if evals > 0 and evals >= len(pop):
             total_time = time() - start_time
-            evals_per_sec = evals / total_time
-            print(f'evals={evals}')
-            print(f'evals_per_sec={evals_per_sec}')
-            print(f'waiting_qsize={waiting.qsize()}')
-            print(f'nursery_qsize={nursery.qsize()}')
-            print(f'bukcets={len(pop.pop_dict)}')
-            print(f'fitness={pop.get(0).fitness}')
+            # print(f'waiting_qsize={waiting.qsize()}')
+            # print(f'nursery_qsize={nursery.qsize()}')
+            gen = StatsGen(
+                total_time,
+                evals,
+                pop.get(0).fitness,
+                len(pop.pop_dict)
+            )
+            evals = 0
+            start_time = time()
+            pipe_to.send(Message(MessageType.GEN, gen))
         if not waiting.full() and not nursery.full():
-            # try some batching
             couples = []
             for n in range(batch_size):
                 mom = pop.get(pop.rand_index())
@@ -45,8 +48,95 @@ def manager(pop, terminate, waiting, nursery, pipe_to, batch_size):
                 pop.pop(len(pop) - pop.rand_index() - 1)
                 pop.add(child)
             evals += batch_size
-    print(f'Done evals={evals}, fitness={pop.get(0).fitness}')
-    pipe_to.send(pop)
+    pipe_to.send(Message(MessageType.DONE, pop))
+
+
+class StatsGen:
+    headers = [
+        'elapsed time',
+        'evals',
+        'evals/sec',
+        'best fitness',
+        'fitness classes'
+    ]
+
+    def __init__(
+            self,
+            total_time,
+            evaluations,
+            best_fitness,
+            fitness_classes
+        ):
+        self.time_elapsed = total_time
+        self.evaluations = evaluations
+        self.best_fitness = best_fitness
+        self.fitness_classes = fitness_classes
+    
+    def __str__(self):
+        s = ''
+        s += str(self.time_elapsed) + ', '
+        s += str(self.evaluations) + ', '
+        s += str(self.evaluations / self.time_elapsed) + ', '
+        s += str(self.best_fitness) + ', '
+        s += str(self.fitness_classes)
+        return s
+
+
+class MessageType:
+    DONE = 'done'
+    GEN = 'gen'
+
+
+class Message:
+
+    def __init__(self, message_type, payload):
+        self.type = message_type
+        self.payload = payload
+
+
+class Stats:
+
+    def __init__(self, batch_size=1, num_workers=1):
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.generations = blist([])
+        self.total_time = 0
+        self.start_time = None
+
+    def start(self, fn=None):
+        self.start_time = time()
+        if fn:
+            fn(self)
+
+    def end(self, fn=None):
+        self.total_time = time() - self.start_time
+        if fn:
+            fn(self)
+        else:
+            print(self)
+
+    def add_gen(self, gen, fn=None):
+        self.generations.append(gen)
+        if fn:
+            fn(self)
+        else:
+            print(gen)
+
+    def __str__(self):
+        s = '# STATISTICS\n\n'
+        s += '## GENERATIONS\n'
+        s += '#, '
+        for header in StatsGen.headers:
+            s += str(header) + ', '
+        s = s[:-2] + '\n'
+        for i, gen in enumerate(self.generations):
+            s += str(i) + ', ' + str(gen) + '\n'
+        s += '\n'
+        s += '* batch_size: ' + str(self.batch_size) + '\n'
+        s += '* num_workers: ' + str(self.num_workers) + '\n'
+        s += '* num_generations: ' + str(len(self.generations)) + '\n'
+        s += '* total time: ' + str(self.total_time) + '\n\n'
+        return s
 
 
 class Population:
@@ -64,51 +154,48 @@ class Population:
     def evolve(self, terminate, num_workers=cpu_count(), batch_size=None):
         if not batch_size:
             batch_size = ceil(log(len(self)) / 2.0)
-        print(f'batch_size={batch_size}')
-        start_time = time()
+        stats = Stats(batch_size=batch_size, num_workers=num_workers)
+        stats.start()
         waiting = Queue(maxsize=2*num_workers)
         nursery = Queue(maxsize=2*num_workers)
         pipe_to, pipe_from = Pipe()
-        print(f'num_workers={num_workers}')
         pool = Pool(num_workers, evaluator, (self.chrm, waiting, nursery))
         mgr = Process(target=manager, args=(self, terminate, waiting, nursery, pipe_to, batch_size))
         mgr.start()
         new_pop = None
         while not new_pop:
-            new_pop = pipe_from.recv()
-        total_sec = time() - start_time
-        total_min = total_sec / 60
-        print(f'total_sec={total_sec}')
-        print(f'total_min={total_min}')
-        return new_pop
+            message = pipe_from.recv()
+            if message.type == MessageType.DONE:
+                new_pop = message.payload
+            if message.type == MessageType.GEN:
+                stats.add_gen(message.payload)
+        stats.end()
+        return stats
 
     def sync_evolve(self, terminate):
-        start_time = time()
+        stats = Stats()
+        stats.start()
         gen_start = time()
-        gen = 0
         evals = 0
         while not terminate(evals, self):
-            if evals % len(self) == 0:
-                gen_total = time() - gen_start
-                gen_start = time()
-                evals_per_sec = len(self) / gen_total
-                print(f'gen={gen}')
-                print(f'gen_totals={gen_total}')
-                print(f'evals={evals}')
-                print(f'evals_per_sec={evals_per_sec}')
-                print(f'fitness={self.get(0).fitness}')
-                gen += 1
             mom = self.get(self.rand_index())
             dad = self.get(self.rand_index())
             child = self.chrm.mate(mom, dad)
             self.pop(len(self) - self.rand_index() - 1)
             self.add(child)
             evals += 1
-        total_sec = time() - start_time
-        total_min = total_sec / 60
-        print(f'total_sec={total_sec}')
-        print(f'total_min={total_min}')
-        return self
+
+            if evals % len(self) == 0:
+                gen_total = time() - gen_start
+                gen_start = time()
+                stats.add_gen(StatsGen(
+                    gen_total,
+                    len(self),
+                    self.get(0).fitness,
+                    len(self.pop_dict)
+                ))
+        stats.end()
+        return stats
 
     def rand_index(self):
         ex = 4 * log(len(self))
